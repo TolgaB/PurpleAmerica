83aa2470f70e617f0b48eb55e27ef6c1ef8baa3c
Tolgab <Mathman290@gmail.com>
2015-11-21 18:21:55 -0800

    state switcher

--- src/net/demipixel/purple/Main.java
+++ src/net/demipixel/purple/Main.java
@@ -62,9 +62,9 @@ public class Main extends Canvas implements Runnable {
 			Region state = USA.getSubByName(fullname);
 			if (state == null) {
 				if (!mini.equals("AK") && !mini.equals("HI")) {
-					System.out.println("COULDN'T FIND STATE FOR " + mini + ":" + fullname);
+				//	System.out.println("COULDN'T FIND STATE FOR " + mini + ":" + fullname);
 				} else {
-					System.out.println("Ignoring state " + mini);
+					//System.out.println("Ignoring state " + mini);
 				}
 				continue;
 			}
@@ -89,7 +89,12 @@ public class Main extends Canvas implements Runnable {
 		//Using the election data
 		for (int i = 0; i < electionDataList.size(); i++) {
 			Stack<String> dataStack = electionDataList.get(i);
-			System.out.println(dataStack.pop());
+			for (int r = 0; r < dataStack.size(); r++) {
+				if (r == 0) {
+					String stateInitials = dataStack.pop().substring(0, Math.min(dataStack.pop().length(), 2));
+					System.out.println(GenerateStateName(stateInitials));
+				}
+			}
 			
 		}
 		
@@ -98,7 +103,85 @@ public class Main extends Canvas implements Runnable {
 		}
 	}
 	
-
+	private String GenerateStateName(String initial) {
+		Map<String, String> states = new HashMap<String, String>();
+		states.put("Alabama","AL");
+		states.put("Alaska","AK");
+		states.put("Alberta","AB");
+		states.put("American Samoa","AS");
+		states.put("Arizona","AZ");
+		states.put("Arkansas","AR");
+		states.put("Armed Forces (AE)","AE");
+		states.put("Armed Forces Americas","AA");
+		states.put("Armed Forces Pacific","AP");
+		states.put("British Columbia","BC");
+		states.put("California","CA");
+		states.put("Colorado","CO");
+		states.put("Connecticut","CT");
+		states.put("Delaware","DE");
+		states.put("District Of Columbia","DC");
+		states.put("Florida","FL");
+		states.put("Georgia","GA");
+		states.put("Guam","GU");
+		states.put("Hawaii","HI");
+		states.put("Idaho","ID");
+		states.put("Illinois","IL");
+		states.put("Indiana","IN");
+		states.put("Iowa","IA");
+		states.put("Kansas","KS");
+		states.put("Kentucky","KY");
+		states.put("Louisiana","LA");
+		states.put("Maine","ME");
+		states.put("Manitoba","MB");
+		states.put("Maryland","MD");
+		states.put("Massachusetts","MA");
+		states.put("Michigan","MI");
+		states.put("Minnesota","MN");
+		states.put("Mississippi","MS");
+		states.put("Missouri","MO");
+		states.put("Montana","MT");
+		states.put("Nebraska","NE");
+		states.put("Nevada","NV");
+		states.put("New Brunswick","NB");
+		states.put("New Hampshire","NH");
+		states.put("New Jersey","NJ");
+		states.put("New Mexico","NM");
+		states.put("New York","NY");
+		states.put("Newfoundland","NF");
+		states.put("North Carolina","NC");
+		states.put("North Dakota","ND");
+		states.put("Northwest Territories","NT");
+		states.put("Nova Scotia","NS");
+		states.put("Nunavut","NU");
+		states.put("Ohio","OH");
+		states.put("Oklahoma","OK");
+		states.put("Ontario","ON");
+		states.put("Oregon","OR");
+		states.put("Pennsylvania","PA");
+		states.put("Prince Edward Island","PE");
+		states.put("Puerto Rico","PR");
+		states.put("Quebec","PQ");
+		states.put("Rhode Island","RI");
+		states.put("Saskatchewan","SK");
+		states.put("South Carolina","SC");
+		states.put("South Dakota","SD");
+		states.put("Tennessee","TN");
+		states.put("Texas","TX");
+		states.put("Utah","UT");
+		states.put("Vermont","VT");
+		states.put("Virgin Islands","VI");
+		states.put("Virginia","VA");
+		states.put("Washington","WA");
+		states.put("West Virginia","WV");
+		states.put("Wisconsin","WI");
+		states.put("Wyoming","WY");
+		states.put("Yukon Territory","YT");
+		HashMap<String, String> reversedHashMap = new HashMap<String, String>();
+		for (String i : states.keySet()) {
+		    reversedHashMap.put(states.get(i), i);
+		}
+		return reversedHashMap.get(initial);
+	}
 	
 	private Region generateRegions(Region region, String file) throws IOException {
 		ArrayList<String> lines = readFile(file);
@@ -120,8 +203,8 @@ public class Main extends Canvas implements Runnable {
 					if (positionDown == 0) { // Create region with name
 						current.name = l;
 						if (reverseHash.get(current.name) == null && file.equals("./purple/USA.txt")) {
-							System.out.println("NO HASH FOR " + current.name);
-							System.out.println(reverseHash.get("California"));
+							//System.out.println("NO HASH FOR " + current.name);
+							//System.out.println(reverseHash.get("California"));
 						}
 						region.subregions[stateNum] = current;
 						stateNum++;
